f75d361c9195c4197d5d1d400640d1dba9fac6f5
firdausnanda <firdausnanda46@gmail.com>
2026-05-07 09:42:15 +0700

    [FIX] Enable Auto Slide

--- app/Models/User.php
+++ app/Models/User.php
@@ -13,7 +13,7 @@ use Spatie\Activitylog\LogOptions;
 
 class User extends Authenticatable
 {
-    /** @use HasFactory<\Database\Factories\UserFactory> */
+
     use HasFactory, Notifiable, HasRoles, Impersonate, LogsActivity;
 
     public function getActivitylogOptions(): LogOptions
