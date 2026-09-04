c35376aaa8e65f4452d0b6f56a170859fa4df692
firdausnanda <firdausnanda46@gmail.com>
2026-09-04 23:44:23 +0700

    [FIX] Force to HTTPS in Production Mode

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,6 +21,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        if (config('app.env') === 'production') {
+            \Illuminate\Support\Facades\URL::forceScheme('https');
+        }
+
         if (class_exists(LogViewer::class)) {
             LogViewer::auth(function ($request) {
                 return $request->user()
